1391d7dc0422fccee110a2d82c8be7738d1f08dd
GeorgeKelly <george.kelly@digital.justice.gov.uk>
2020-03-19 14:35:16 +0000

    testing

--- tests/data/junk.py
+++ tests/data/junk.py
@@ -1,5 +1,6 @@
 import yaml
 import json
+import os
 from jsonschema import validate as json_validate
 
 with open("tests/data/example_config.yaml", "r") as y:
@@ -9,31 +10,26 @@ json.dumps(config)
 
 with open("config-schema.json") as f:
     config_schema = json.load(f)
-
-
+config_path = os.path.join(".", "config.yaml")
+config_path
 json_validate(config, config_schema)
-
-def load_and_validate_config():
+def load_and_validate_config(file_name="config.yaml", path="."):
     """
     Loads and validates the config
     """
     # load yaml or json
+    config_path = os.path.join()
     if os.path.isfile("config.yaml"):
         ext = "yaml"
     elif os.path.isfile("config.yml"):
         ext = "yml"
-    elif os.path.isfile("config.json"):
-        ext = "json"
     else:
         raise FileNotFoundError(
-            "Expecting a file with the name config.json or config.yaml in working dir."
+            "Expecting a file with the name config.json, config.yaml config.yml) in working dir."
         )
 
     with open(f"config.{ext}", "r") as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-
-    with open("config-schema.json") as f:
-        config_schema = json.load(f)
+        config = yaml.safe_load(f)
 
     json_validate(config, config_schema)
 
