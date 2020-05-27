6ff83b659837eb33fd98f4d7676731b57e774747
Thomas Hirsch <thomashirsch@JLHC.local>
2020-05-27 15:26:37 +0100

    fix log_setup

--- python_scripts/logging_functions.py
+++ python_scripts/logging_functions.py
@@ -1,22 +1,31 @@
 import logging
+from datetime import datetime
+import io
 
+import boto3
+
+s3_client = boto3.client("s3")
 
 def logging_setup():
     log_name = "log.csv"
-    log_name_timestamped = f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} log.csv"
+    log_name_timestamped = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} log.csv"
     
     log = logging.getLogger(log_name)
     log_stringio = io.StringIO()
     handler = logging.StreamHandler(log_stringio)
     log.addHandler(handler)
     log.setLevel(logging.INFO)
-    log.formatter = logging.Formatter(format="%(levelname)s,%(asctime)s,%(message)s",datefmt="%Y-%m-%d %H:%M:%S")
+    log.formatter = logging.Formatter(fmt="%(levelname)s,%(asctime)s,%(message)s",datefmt="%Y-%m-%d %H:%M:%S")
 
-    return log
+    output = {"log": log,
+              "log_name_timestamped": log_name_timestamped,
+              "log_stringio": log_stringio}
+
+    return output
 
 
 def upload_logs(bucket, key, body):
     s3_client.put_object(Body=body, Bucket=bucket, Key=key)
 
-def write_to_log(logger, identifier, message, level='INFO'):
-    logger.log(getattr(logging, level), f"{identifier}, {message}")
+def write_to_log(log, identifier, message, level='INFO'):
+    log.log(getattr(logging, level), f"{identifier}, {message}")
--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,7 +1,7 @@
 from goodtables import validate
 from gluejobutils import s3
 
-from functions import load_and_validate_config, validate_data, upload_log
+from functions import load_and_validate_config, validate_data
 
 import logging
 from datetime import datetime
@@ -9,14 +9,20 @@ import atexit
 import io
 from logging_functions import upload_logs, logging_setup
 
+
 def main():
     # set up logging
-    logging_setup()
-    
+    log = logging_setup()
+
     config = load_and_validate_config()
     validate_data(config)
 
-    atexit.register(upload_logs, body=log_stringio.get_value(), bucket=config["log-base-path"], key=log_name_timestamped)
+    atexit.register(
+        upload_logs,
+        body=log["log_stringio"].getvalue(),
+        bucket=config["log-base-path"],
+        key=log["log_name_timestamped"],
+    )
 
 
 if __name__ == "__main__":
