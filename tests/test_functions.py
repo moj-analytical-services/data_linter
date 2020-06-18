89b42eddd3553867e911e1227a9393350ae3959e
GeorgeKelly <george.kelly@digital.justice.gov.uk>
2020-06-18 16:17:50 +0100

    test data moved

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -7,7 +7,7 @@ from data_linter.validation import convert_meta_to_goodtables_schema
 
 
 def test_convert_meta_type_to_goodtable_type():
-    with open("tests/data/basic_meta_data.json") as f:
+    with open("tests/data/inputs/basic_meta_data.json") as f:
         meta = json.load(f)
 
     gt_schema = convert_meta_to_goodtables_schema(meta)
