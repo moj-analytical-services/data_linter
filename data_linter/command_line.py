e4d9516a407808c8103ac324b60501ae82445db4
Thomas Hirsch <thomashirsch@JLHC.local>
2020-07-08 17:19:15 +0100

    add cli interface

---
+++ data_linter/command_line.py
@@ -0,0 +1,13 @@
+from data_linter import run_validation
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config_path", help="Path to a config.yaml")
+    args = parser.parse_args()
+    config_path = args.config
+
+    run_validation(config_path)
+
+if __name__ == "__main__":
+    main()
