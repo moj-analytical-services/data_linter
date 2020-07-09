4199317bd043883b5d2e3d2703ec6f883ca17d28
Thomas Hirsch <thomashirsch@JLHC.local>
2020-07-09 14:49:10 +0100

    fix typo

--- data_linter/command_line.py
+++ data_linter/command_line.py
@@ -5,7 +5,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config_path", help="Path to a config.yaml")
     args = parser.parse_args()
-    config_path = args.config
+    config_path = args.config_path
 
     run_validation(config_path)
 
