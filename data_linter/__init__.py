657354bb974e02e9a758924d36ed9b6d619140f8
Karik Isichei <karik.isichei@gmail.com>
2020-07-13 17:20:30 +0100

    This is how it should work...

--- data_linter/__init__.py
+++ data_linter/__init__.py
@@ -4,7 +4,6 @@ import io
 import sys
 
 import poetry_version
-
 from data_linter.validation import run_validation
 
 __version__ = poetry_version.extract(source_file=__file__)
--- data_linter/command_line.py
+++ data_linter/command_line.py
@@ -1,11 +1,9 @@
-
-from data_linter.validation import run_validation
-from data_linter.constants import version
+from data_linter import run_validation, __version__
 import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--version", action="version", version='%(prog)s {version}'.format(version=version))
+    parser.add_argument("-v", "--version", action="version", version='%(prog)s {version}'.format(version=__version__))
     parser.add_argument("-c", "--config_path", help="Path to a config.yaml")
     args = parser.parse_args()
 
