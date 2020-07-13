05b464a929b64664b30d9997c884f2f6f10824e9
GeorgeKelly <george.kelly@digital.justice.gov.uk>
2020-07-13 17:40:26 +0100

    testing

--- data_linter/__init__.py
+++ data_linter/__init__.py
@@ -7,4 +7,6 @@ import poetry_version
 
 from data_linter.validation import run_validation
 
-__version__ = poetry_version.extract(source_file=__file__)
+__version__ = "1.0.0"
+
+# poetry_version.extract(source_file=__file__)
