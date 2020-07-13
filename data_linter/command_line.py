af97e2cdd125aca12c24f29da31134bedea0940c
Karik Isichei <karik.isichei@gmail.com>
2020-07-13 15:54:07 +0100

    Another attempt

--- data_linter/command_line.py
+++ data_linter/command_line.py
@@ -1,11 +1,14 @@
 from data_linter import run_validation, __version__
 import argparse
 
-if __name__ == "__main__":
-
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", action="version", version='%(prog)s {version}'.format(version=__version__))
     parser.add_argument("-c", "--config_path", help="Path to a config.yaml")
 
     args = parser.parse_args()
     run_validation(args.config_path)
+
+
+if __name__ == "__main__":
+    main()
