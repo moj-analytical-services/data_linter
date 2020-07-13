d6b086d5f464eb309500d9a2227408ee3847631c
GeorgeKelly <george.kelly@digital.justice.gov.uk>
2020-07-13 18:03:46 +0100

    last attempt

--- data_linter/command_line.py
+++ data_linter/command_line.py
@@ -1,19 +1,17 @@
-from data_linter import run_validation, __version__
-import poetry_version
+import data_linter as dl
 import argparse
 
 def main():
-    __version__ = poetry_version.extract(source_file=__file__)
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-v",
         "--version",
         action="version",
-        version="%(prog)s {version}".format(version=__version__),
+        version="%(prog)s {version}".format(version=dl.__version__),
     )
     parser.add_argument("-c", "--config_path", help="Path to a config.yaml")
     args = parser.parse_args()
-    run_validation(args.config_path)
+    dl.run_validation(args.config_path)
 
 
 if __name__ == "__main__":
