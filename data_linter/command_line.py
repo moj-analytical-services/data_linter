fae3fc83b00d9285893eeba9a6983507ef52e448
Karik Isichei <karik.isichei@gmail.com>
2020-07-13 17:28:23 +0100

    black and working apparently

--- data_linter/command_line.py
+++ data_linter/command_line.py
@@ -1,12 +1,19 @@
-from data_linter import run_validation, __version__
+from data_linter import __version__
+from data_linter.validation import run_validation
+
 import argparse
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--version", action="version", version='%(prog)s {version}'.format(version=__version__))
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="%(prog)s {version}".format(version=__version__),
+    )
     parser.add_argument("-c", "--config_path", help="Path to a config.yaml")
     args = parser.parse_args()
-
     run_validation(args.config_path)
 
 
