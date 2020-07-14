17996841f7b590c2aaa644bf685bde23ebb6badd
Karik Isichei <karik.isichei@gmail.com>
2020-07-14 12:11:12 +0100

    Think this should work for both 3.7 and 3.8

--- data_linter/__init__.py
+++ data_linter/__init__.py
@@ -3,7 +3,11 @@ import atexit
 import io
 import sys
 
-from importlib.metadata import version
+if sys.version_info[1] < 8:
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
+    
 from data_linter.validation import run_validation
 
 __version__ = version("data_linter")
