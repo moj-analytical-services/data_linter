89f96398c445fb3eec661b985a1165eb725a323b
GeorgeKelly <george.kelly@digital.justice.gov.uk>
2020-07-14 10:20:54 +0100

    trying to revert back to poetry version

--- data_linter/__init__.py
+++ data_linter/__init__.py
@@ -3,7 +3,7 @@ import atexit
 import io
 import sys
 
-from importlib.metadata import version
+import poetry_version
 from data_linter.validation import run_validation
 
-__version__ = version("data_linter")
+__version__ = poetry_version.extract(source_file=__file__)
