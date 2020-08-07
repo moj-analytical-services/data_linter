3f7fad48f479fc798806c191aa51587f28727f34
Karik Isichei <karik.isichei@gmail.com>
2020-08-07 13:42:13 +0100

    Making docstring order of args match that of function input

--- data_linter/validation.py
+++ data_linter/validation.py
@@ -237,9 +237,9 @@ def _read_data_and_validate(
 
     Args:
         filepath (str): String pointing to file path can be S3, local, etc
+        schema (dict): the goodtables schema
         table_params (dict): Table params dictionary
         metadata (dict): The metadata for the table
-        schema (dict): the goodtables schema
     """
 
     # Get the first line from the file if expecting a header
