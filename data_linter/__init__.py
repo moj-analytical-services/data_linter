99e274f4d4b2c4b95e5c8626202c10bf25f852be
Karik Isichei <karik.isichei@digital.justice.gov.uk>
2020-08-07 13:35:40 +0100

    Changed to case insensitive (#59)
    Changed to case insensitive

--- data_linter/__init__.py
+++ data_linter/__init__.py
@@ -7,7 +7,7 @@ if sys.version_info[1] < 8:
     from importlib_metadata import version
 else:
     from importlib.metadata import version
-    
+
 from data_linter.validation import run_validation
 
 __version__ = version("data_linter")
