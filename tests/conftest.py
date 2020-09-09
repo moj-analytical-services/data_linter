37558a4c12ebbd797e6f343d4a929344ce6059a6
GeorgeKelly <george.kelly@digital.justice.gov.uk>
2020-09-09 15:40:35 +0100

    thomas's comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from moto import mock_s3
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def aws_credentials():
     """Mocked AWS Credentials for moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
@@ -14,13 +14,13 @@ def aws_credentials():
     os.environ["AWS_SESSION_TOKEN"] = "testing"
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def s3(aws_credentials):
     with mock_s3():
         yield boto3.resource("s3", region_name="eu-west-1")
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def s3_client(aws_credentials):
     with mock_s3():
         yield boto3.client("s3", region_name="eu-west-1")
