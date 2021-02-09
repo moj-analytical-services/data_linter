889e80e68d3d04a7d87d96a022fa1ab70f77d9e2
Stephen <stephen@192.168.1.145>
2021-02-09 11:05:01 +0000

    fixed flake8/test issues (#121)

--- data_linter/__init__.py
+++ data_linter/__init__.py
@@ -1,9 +1,8 @@
 import sys
-import poetry_version
 
 if sys.version_info[1] < 8:
     from importlib_metadata import version
 else:
     from importlib.metadata import version
 
-__version__ = poetry_version.extract(source_file=__file__)
+__version__ = version("data_linter")
