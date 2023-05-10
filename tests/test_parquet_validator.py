25b7eef44b11c3867e642a6892f639320c16b924
gwionap <gwion.aprhobat@justice.gov.uk>
2023-05-10 10:13:06 +0100

    Updated parquet validator test, changelog and versioning.

--- tests/test_parquet_validator.py
+++ tests/test_parquet_validator.py
@@ -4,8 +4,8 @@ import awswrangler as wr
 import boto3
 import pytest
 from mojap_metadata import Metadata
+from mojap_metadata.converters.arrow_converter import ArrowConverter
 from moto import mock_s3
-from pyarrow.parquet import read_schema
 
 import data_linter.validators.parquet_validator as pqv
 from tests.helpers import mock_get_file
@@ -15,13 +15,21 @@ bucket = "dummy-bucket"
 
 @mock_s3
 @pytest.mark.parametrize(
-    "filepath, mock_s3",
+    "filepath, mock_s3, meta_path",
     [
-        ("tests/data/parquet_validator/table1.parquet", True),
-        ("tests/data/parquet_validator/table1.parquet", False),
+        (
+            "tests/data/parquet_validator/table1.parquet",
+            True,
+            "tests/data/parquet_validator/meta_data/table1_pass.json",
+        ),
+        (
+            "tests/data/parquet_validator/table1.parquet",
+            False,
+            "tests/data/parquet_validator/meta_data/table1_pass.json",
+        ),
     ],
 )
-def test_parquet_schema_reader(filepath, mock_s3, monkeypatch):
+def test_parquet_schema_reader(filepath, mock_s3, meta_path, monkeypatch):
     if mock_s3:
 
         s3_client = boto3.client("s3")
@@ -40,10 +48,15 @@ def test_parquet_schema_reader(filepath, mock_s3, monkeypatch):
     _ = monkeypatch.setattr(pqv, "S3FileSystem", mock_get_file)
 
     schema = pqv.ParquetValidator._read_schema(full_path)
+    ac = ArrowConverter()
 
-    expected_schema = read_schema(filepath).remove_metadata()
+    meta = ac.generate_to_meta(schema)
+    column_names = sorted(meta.column_names)
 
-    assert expected_schema == schema
+    expected_meta = Metadata.from_json(meta_path)
+    expected_column_names = sorted(expected_meta.column_names)
+
+    assert column_names == expected_column_names
 
 
 @pytest.mark.parametrize(
